466f082a3a22978edc5f7a402706d98dbf25cc31
Dante <dante@MGMT-PC.lan.inferno>
2021-04-03 18:06:11 +0200

    first spring, first ver SC w/ tests

--- brownie/contracts/DeSign.sol
+++ brownie/contracts/DeSign.sol
@@ -4,7 +4,21 @@ pragma solidity ^0.8.0;
 import "OpenZeppelin/openzeppelin-contracts@4.0.0/contracts/access/Ownable.sol";
 
 contract DeSign is Ownable{
-	function test() public view returns (uint){
-		return 121;
+
+	struct IndexEntry {
+		bytes32 _documentBlockHash;
+		bytes _documentBlockLink;
+		uint _expirationTimestamp;
+	}
+
+	mapping (bytes32 => IndexEntry) private index;
+
+	function indexMerkleRoot(bytes32 _indexHash, bytes32 documentBlockHash, bytes memory documentBlockLink, uint _daysBeforeExpiration) public onlyOwner {
+		index[_indexHash] = IndexEntry(documentBlockHash, documentBlockLink, block.timestamp + (_daysBeforeExpiration * 86400));
+	}
+
+	function getIndexData(bytes32 _indexHash) public view returns (bytes32, bytes memory, uint){
+		IndexEntry memory entry = index[_indexHash];
+		return (entry._documentBlockHash, entry._documentBlockLink, entry._expirationTimestamp - block.timestamp);
 	}
 }
--- brownie/tests/DeSign_test.py
+++ brownie/tests/DeSign_test.py
@@ -3,11 +3,21 @@ from brownie.convert import to_bytes
 from brownie.exceptions import *
 import brownie
 import pytest
+import hashlib
 
 @pytest.fixture(autouse=True)
 def setupVariables():
 	global owner
 	owner = accounts[0]
+	global indexHash 
+	indexHash = to_bytes(hashlib.sha256("01/01/1980".encode()).hexdigest())
+	global merkleRoot 
+	merkleRoot = to_bytes(hashlib.sha256("THIS IS A BLOCK OF DOCUMENTS".encode()).hexdigest())
+	global validityTime
+	validityTime = 365
+	global documentBlockLink
+	documentBlockLink = "SQL SIGMA BALLS"
+
 
 @pytest.fixture
 def DeSignContract():
@@ -15,3 +25,26 @@ def DeSignContract():
 
 def testCheckOwner(DeSignContract):
 	assert DeSignContract.owner() == owner
+
+def testIndexMerkleRoot(DeSignContract):
+	print("index hash : " + indexHash.hex())
+	print("merkle root / document block hash : " + merkleRoot.hex())
+	DeSignContract.indexMerkleRoot(indexHash, merkleRoot, bytes(documentBlockLink, 'utf-8'), validityTime, {"from":owner})
+	try:
+		DeSignContract.indexMerkleRoot(indexHash, merkleRoot, bytes(documentBlockLink, 'utf-8'), validityTime, {"from":accounts[1]})
+		assert False
+	except(VirtualMachineError):
+		assert True
+	else:
+		assert False
+
+
+	########################################################
+	####test merkle root & document block link retreival####
+	########################################################
+
+	indexData = DeSignContract.getIndexData(indexHash)
+	print(indexData)
+	assert indexData[0] == "0x" + merkleRoot.hex()
+	assert indexData[1].decode('utf-8') == documentBlockLink
+	assert indexData[2] <= validityTime * 86400
